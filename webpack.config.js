7f76954fbbee0bf2436bf77c30058e14836a4cf3
GraySergey <xgortx@gmail.com>
2019-03-01 21:42:09 +0200

    Корректирует настройку слежения за html файлом

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,6 @@ const path = require(`path`);
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  plugins: [new HtmlWebpackPlugin()],
-  template: `./public/index.html`,
-  inject: false,
-};
-
-module.exports = {
   mode: `development`,
   entry: `./src/main.js`,
   output: {
@@ -16,9 +10,16 @@ module.exports = {
     path: path.join(__dirname, `public`)
   },
   devtool: `source-map`,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `./public/index.html`,
+      inject: false
+    })
+  ],
   devServer: {
     contentBase: path.join(__dirname, `public`),
     publicPath: 'http://localhost:8080/',
+    watchContentBase: true,
     hot: true,
     compress: true,
   },
